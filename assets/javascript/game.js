0c70c3e3d50e78fb89738d101f3918241feffeab
vivgarcia <viv.garcia88@gmail.com>
2020-02-18 13:20:46 -0600

    upated some styles and starting js

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//generate randomNumber and display to the DOM
+var randomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+$('#randomNumber').html(randomNumber);
+
+//define variables
+var ruby = Math.floor(Math.random() * 12) + 1;
+var citrine = Math.floor(Math.random() * 12) + 1;
+var amethyst = Math.floor(Math.random() * 12) + 1;
+var carnelian = Math.floor(Math.random() * 12) + 1;
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
