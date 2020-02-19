d8b913f8b8866314a442869b4f615d16ce6eca33
vivgarcia <viv.garcia88@gmail.com>
2020-02-19 17:45:49 -0600

    update functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,12 +10,21 @@ var carnelian = Math.floor(Math.random() * 12) + 1;
 var wins = 0;
 var losses = 0;
 var totalScore = 0;
-
+//write score to the DOM
 $("#totalScore").text(totalScore);
-
+//functions for game play
+function someFunction(){
+    if(totalScore === randomNumber){
+        console.log("won");
+    }else if(totalScore >= randomNumber){
+        console.log("hey");
+    }
+}
+//assign gems click functions and add their numbers to total score
 $("#rubyGem").on("click", function(){
     totalScore = totalScore + ruby;
     $("#totalScore").text(totalScore);
+    someFunction();
 })
 
 $("#citrineGem").on("click", function(){
@@ -32,3 +41,4 @@ $("#carnelianGem").on("click", function(){
     totalScore = totalScore + carnelian;
     $("#totalScore").text(totalScore);
 })
+
